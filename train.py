eda388c9756f8add4d0c326c8a515e9ef8d1bf7f
Flavio Piccoli <flavio.piccoli@unimib.it>
2021-01-22 22:47:53 +0100

    Using smaller network.

--- train.py
+++ train.py
@@ -42,6 +42,60 @@ def test(net, ds):
     # return var_loss, tot_loss
 
 
+# # define network
+# class Net(nn.Module):
+#     def __init__(self, dropout=0):
+#         super(Net, self).__init__()
+#         self.b1 = self.block(1,16)
+#         self.b2 = self.block(16,32)
+#         self.b3 = self.block(32,64)
+#         self.b4 = self.block(64,128)
+#         self.b5 = self.block(128,128)
+#         self.b6 = self.block(128,128)
+#         self.b7 = self.block(128,128)
+#         self.b8 = self.block(128,128)
+#         self.b9 = self.block(128,128)
+#         self.b10 = nn.Sequential(
+#             nn.Conv2d(128, 128, kernel_size=(1,4), stride=1, padding=(0,0)),
+#             nn.BatchNorm2d(128),
+#             nn.ReLU(inplace=True),
+#         )
+#         self.dropout = nn.Dropout(p=dropout)
+#         self.final = nn.Sequential(
+#                         nn.Linear(128, 128),
+#                         nn.ReLU(inplace=True),
+#                         nn.Linear(128, 12))
+#
+#
+#     def block(self, ch_in, ch_out, sz=(1,3), st=(1,1), pad=(0,0)):
+#         return nn.Sequential(
+#             nn.Conv2d(ch_in, ch_in, kernel_size=(1,3), stride=1, padding=(0,0)),
+#             nn.BatchNorm2d(ch_in),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(ch_in, ch_out, kernel_size=sz, stride=st, padding=pad),
+#     		nn.BatchNorm2d(ch_out),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=(1,2), stride=(1,2))
+#         )
+#
+#
+#     def forward(self, x):
+#         x = self.b1(x)
+#         x = self.b2(x)
+#         x = self.b3(x)
+#         x = self.b4(x)
+#         x = self.b5(x)
+#         x = self.b6(x)
+#         x = self.b7(x)
+#         x = self.b8(x)
+#         x = self.b9(x)
+#         x = self.b10(x)
+#         x = x.view(x.size(0),-1)
+#         x = self.dropout(x)
+#         x = self.final(x)
+#         return x
+
+
 # define network
 class Net(nn.Module):
     def __init__(self, dropout=0):
@@ -56,7 +110,7 @@ class Net(nn.Module):
         self.b8 = self.block(128,128)
         self.b9 = self.block(128,128)
         self.b10 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=(1,4), stride=1, padding=(0,0)),
+            nn.Conv2d(128, 128, kernel_size=(1,5), stride=1, padding=(0,0)),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
         )
@@ -69,13 +123,9 @@ class Net(nn.Module):
 
     def block(self, ch_in, ch_out, sz=(1,3), st=(1,1), pad=(0,0)):
         return nn.Sequential(
-            nn.Conv2d(ch_in, ch_in, kernel_size=(1,3), stride=1, padding=(0,0)),
-            nn.BatchNorm2d(ch_in),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(ch_in, ch_out, kernel_size=sz, stride=st, padding=pad),
-    		nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(1,2), stride=(1,2))
+            nn.Conv2d(ch_in, ch_out, kernel_size=(1,5), stride=2, padding=(0,0)),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
         )
 
 
