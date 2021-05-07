a54022ae327bfc384a7680e54d1c446edd272fac
Flavio Piccoli <flavio.piccoli@unimib.it>
2021-05-07 17:02:15 +0200

    Updated grid_search.

--- grid_search.py
+++ grid_search.py
@@ -9,13 +9,14 @@ from pprint import pprint
 
 
 
-def train_grid_point(config, data_dir=None, max_epochs=50, num_gpus=1): # 5000 10000
+def train_grid_point(config, data_dir=None, max_epochs=5000, num_gpus=1): # 5000 10000
     # define model
     model = Experiment(config)
     # define metrics
     metrics = {}
     for cur_var in config['tgt_vars'] + ['global']:
-        metrics[cur_var] = 'avg/r2/'+cur_var
+        for cur_metric in ['mae','mse','rmse','r2']:
+            metrics[cur_metric + '/' + cur_var] = 'avg/' + cur_metric + '/'+cur_var
     # define tune callback
     callbacks = [
                   TuneReportCallback(metrics, on="validation_end"),
@@ -38,9 +39,9 @@ if __name__ == '__main__':
     ray.init(address='149.132.176.97:6379', _redis_password='5241590000000000')
     # define grid configuration
     config = {
-        'train_csv': '/app/data/lucas_dataset_train.csv',
-        'val_csv': '/app/data/lucas_dataset_val.csv',
-        'test_csv': '/app/data/lucas_dataset_val.csv',
+        'train_csv': '/home/flavio/datasets/LucasLibrary/shared/lucas_dataset_train.csv',
+        'val_csv': '/home/flavio/datasets/LucasLibrary/shared/lucas_dataset_val.csv',
+        'test_csv': '/home/flavio/datasets/LucasLibrary/shared/lucas_dataset_val.csv',
         'src_prefix': 'spc.',
         'batch_size': 2000, # 10000
         'num_workers': 8,
@@ -55,9 +56,9 @@ if __name__ == '__main__':
 
         'learning_rate': tune.grid_search([0.001, 0.0001]),  #0.001,
         'weight_decay': 0.01,
-        'loss': tune.grid_search(['l1','l2']),
+        'loss': tune.grid_search(['l1','l2', 'classification']),
         'val': 'r2',
-        'nbins': 0, # 10
+        'nbins': 10, # 10
         'tgt_vars': ['coarse','clay','silt','sand','pH.in.CaCl2','pH.in.H2O','OC','CaCO3','N','P','K','CEC'],
     }
 
@@ -65,24 +66,27 @@ if __name__ == '__main__':
 
     analysis = tune.run(
         train_grid_point,
-        config=config,
-        metric='global',
-        mode="max",
-        resources_per_trial={"gpu": 1},
-        # local_dir = '/home/flavio/ray_results',
-        local_dir = '/root/ray_results',
-        # to sync with docker
-        sync_config = tune.SyncConfig(
-            sync_to_driver=DockerSyncer
-        )
         name = 'prova3',
+        local_dir = '/home/flavio/ray_results',
+        config = config,
+        metric = 'r2/global',
+        mode = 'max',
+        resources_per_trial = {'cpu': 6, 'gpu': 1},
         resume = False
     )
     # resume = "ERRORED_ONLY"
-
-    best_config = analysis.get_best_config(metric="global")
-    # print("Best config: ", analysis.get_best_config(metric="global"))
-    pprint(best_config)
-    # Get a dataframe for analyzing trial results.
-    df = analysis.dataframe()
-    df.to_csv('grid_results.csv', sep=';', index=False)
+            # local_dir = '/home/flavio/ray_results',
+            # local_dir = '/home/ray/ray_results',
+            # to sync with docker
+            # sync_config = tune.SyncConfig(
+            #     sync_to_driver=DockerSyncer
+            # )
+    try:
+        best_config = analysis.get_best_config(metric="r2/global")
+        # print("Best config: ", analysis.get_best_config(metric="global"))
+        pprint(best_config)
+        # Get a dataframe for analyzing trial results.
+        df = analysis.dataframe()
+        df.to_csv('grid_results_prova3.csv', sep=';', index=False)
+    except:
+        import ipdb; ipdb.set_trace()
